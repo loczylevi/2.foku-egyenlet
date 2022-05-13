2e30ffce46a133114db57c8724fb25368a3ded72
loczylevi <71691777+loczylevi@users.noreply.github.com>
2022-05-13 15:06:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def masodfoku(a,b,c):
-  minusz = -b - (b **2 -4*a*c) ** 0.5
-  osztas = minusz / (2*a)
-  plusz = -b + (b **2 -4*a*c) ** 0.5
-  osztas2 = plusz / (2*a)
-  if osztas or osztas2 < 0:
+  if 2*a < 0:
     return None
-  elif osztas or osztas2 > 0:
+  if 2*a >= 0:
+    minusz = -b - (b **2 -4*a*c) ** 0.5
+    osztas = minusz / (2*a)
+    plusz = -b + (b **2 -4*a*c) ** 0.5
+    osztas2 = plusz / (2*a)
     return osztas, osztas2
 p = masodfoku(7,8,9)
 print(p)
+z = masodfoku(2, -12, 10)
+print(z)
+  
+
